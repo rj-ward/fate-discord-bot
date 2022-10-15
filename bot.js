e4f4ba6bef489caffdc2090dfd7a5564511af4cb
Remy Ward <remyward@protonmail.com>
2022-10-14 21:21:44 -0500

    Add dice roll function

--- bot.js
+++ bot.js
@@ -7,5 +7,16 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function rollDice(modifier) {
+    var total = 0;
+    for (let index = 0; index < 4; index++) {
+        var roll = round(random() * 2) - 1;
+        total += roll
+    };
+
+    var result = total + modifier;
+    return "Result: " + total + " + " + modifier + " = " + result + ".";
+};
+
 //make sure this line is the last line
 client.login(process.env.DISCORD_TOKEN); //login bot using token
