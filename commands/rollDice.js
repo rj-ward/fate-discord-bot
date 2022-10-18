7cb410952165e5948bf2aae6e1aa656e8c02f463
Remy Ward <remyward@protonmail.com>
2022-10-18 11:57:40 -0500

    Add rollDice command logic

--- commands/rollDice.js
+++ commands/rollDice.js
@@ -6,16 +6,22 @@ function rollDice(modifier) {
         var roll = round(random() * 2) - 1;
         total += roll
     };
-
+    
     var result = total + modifier;
     return "Result: " + total + " + " + modifier + " = " + result + ".";
 };
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('roll')
+		.setDescription('Rolls 4dF with a modifier!')
+        .addNumberOption(option =>
+			option
+				.setName('modifier')
+				.setDescription('The modifier to the roll.')),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+        const modifier = interaction.options.getNumber('modifier');
+        var result = rollDice(modifier);
+		await interaction.reply(result);
 	},
 };
