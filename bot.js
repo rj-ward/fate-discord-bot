8ad2623ed1f28ac45a5791e94b186bcebf7e68a7
Remy Ward <remyward@protonmail.com>
2022-10-18 11:31:40 -0500

    correct env call

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const commands = [];
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationCommands(clientId), { body: commands })
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
 
