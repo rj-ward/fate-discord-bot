16ebd9ec6f39d4016ff7afb117a4ab29580d6ea0
Remy Ward <remyward@protonmail.com>
2022-10-19 07:15:51 -0500

    move fuctions to library

--- commands/fatePoints.js
+++ commands/fatePoints.js
@@ -1,74 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: process.env.DB_ADDRESS,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB_DATABASE
-  });
-
-
-function createChar(name, refresh, current) {
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = `INSERT INTO character (name, refresh, currentfp) VALUES (${name}, ${refresh}, ${current})`;
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("1 record inserted");
-          return "Character created!"
-        });
-      });
-};
-
-function findCharID(name) {
-    con.connect(function(err) {
-        if (err) throw err;
-        var sql = "SELECT id FROM character WHERE name = ?"
-        con.query(sql, [name], function (err, result) {
-          if (err) throw err;
-          console.log(result);
-          return result[0].id
-        });
-      });
-};
-
-function adjustFatePoint(id, value) {
-    con.connect(function(err) {
-        if (err) throw err;
-        var sql = "UPDATE character SET currentfp = ? WHERE id = ?";
-        con.query(sql, [value, id], function (err, result) {
-          if (err) throw err;
-          console.log(result.affectedRows + " record(s) updated");
-          return "Total updated to " + value
-        });
-      });
-};
-
-function adjustRefresh(id, value) {
-    con.connect(function(err) {
-        if (err) throw err;
-        var sql = "UPDATE character SET refresh = ? WHERE id = ?";
-        con.query(sql, [value, id], function (err, result) {
-          if (err) throw err;
-          console.log(result.affectedRows + " record(s) updated");
-          return "Total updated to " + value
-        });
-      });
-};
-
-function viewFatePoints(id){
-    con.connect(function(err) {
-        if (err) throw err;
-        var sql = "SELECT currentfp FROM character WHERE name = ?"
-        con.query(sql, [name], function (err, result) {
-          if (err) throw err;
-          console.log(result);
-          return result[0].currentfp
-        });
-      });
-};
 
 
 module.exports = {
@@ -99,16 +30,4 @@ module.exports = {
 		await interaction.reply(result);
 	},
 
-    data: new SlashCommandBuilder()
-		.setName('roll')
-		.setDescription('Rolls 4dF with a modifier!')
-        .addNumberOption(option =>
-			option
-				.setName('modifier')
-				.setDescription('The modifier to the roll.')),
-	async execute(interaction) {
-        const modifier = interaction.options.getNumber('modifier');
-        var result = rollDice(modifier);
-		await interaction.reply(result);
-	},
 };
---
+++ helperFunc.js
@@ -0,0 +1,81 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: process.env.DB_ADDRESS,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_DATABASE
+  });
+
+
+export function createChar(name, refresh, current) {
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = `INSERT INTO character (name, refresh, currentfp) VALUES (${name}, ${refresh}, ${current})`;
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+          return "Character created!"
+        });
+      });
+};
+
+export function findCharID(name) {
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "SELECT id FROM character WHERE name = ?"
+        con.query(sql, [name], function (err, result) {
+          if (err) throw err;
+          console.log(result);
+          return result[0].id
+        });
+      });
+};
+
+export function adjustFatePoint(id, value) {
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "UPDATE character SET currentfp = ? WHERE id = ?";
+        con.query(sql, [value, id], function (err, result) {
+          if (err) throw err;
+          console.log(result.affectedRows + " record(s) updated");
+          return "Total updated to " + value
+        });
+      });
+};
+
+export function adjustRefresh(id, value) {
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "UPDATE character SET refresh = ? WHERE id = ?";
+        con.query(sql, [value, id], function (err, result) {
+          if (err) throw err;
+          console.log(result.affectedRows + " record(s) updated");
+          return "Total updated to " + value
+        });
+      });
+};
+
+export function viewFatePoints(id){
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "SELECT currentfp FROM character WHERE name = ?"
+        con.query(sql, [name], function (err, result) {
+          if (err) throw err;
+          console.log(result);
+          return result[0].currentfp
+        });
+      });
+};
+
+export function rollDice(modifier) {
+    var total = 0;
+    for (let index = 0; index < 4; index++) {
+        var roll = Math.round(Math.random() * 2) - 1;
+        total += roll
+    };
+
+    var result = total + modifier;
+    return "Result: " + total + " + " + modifier + " = " + result + ".";
+};
