98dd3c8df1b7e128bf9e6e0ea4e0b954178a2e01
Remy Ward <remyward@protonmail.com>
2022-10-23 09:03:22 -0500

    Add spend fate point command.

---
+++ commands/spend.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const func = require("../helperFunc")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('spend')
+		.setDescription('Spend a fate point.')
+        .addStringOption(option =>
+			option
+				.setName('charname')
+				.setDescription('Character Name')
+                .setRequired(true))
+        ,
+	async execute(interaction) {
+        await interaction.deferReply();
+        const name = interaction.options.getString('charname');
+        const fatePoints = func.viewFatePoints(name);
+        var success = func.adjustFatePoint(fatePoints - 1);
+        if (success) {
+            await interaction.editReply("You spent a fate point!");
+        } else {
+            await interaction.editReply("Something went wrong...");
+        };
+		
+        
+	},
+
+};
